372839591d9444b682a51e12342d1366bdc04ccb
sanjus2502 <sanjus2502@gmail.com>
2024-01-03 14:20:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const decreasebtn = document.getElementById("decreasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const increasebtn = document.getElementById("increasebtn");
+let count = 0;
+
+increasebtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreasebtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetbtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
